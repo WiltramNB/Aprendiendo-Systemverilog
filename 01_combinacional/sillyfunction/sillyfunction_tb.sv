3f0245de9025ee5165a7f60fbf99fd5afba8ab40
WiltramNB <medelmartinv12@gmail.com>
2026-06-22 17:48:09 -0400

    1

---
+++ 01_combinacional/sillyfunction/sillyfunction_tb.sv
@@ -0,0 +1,23 @@
+module testbench;
+    logic a, b, c, y;
+
+    // Instanciar el DUT
+    sillyfunction dut(.a(a), .b(b), .c(c), .y(y));
+
+    initial begin
+        $display("a b c | y");
+        $display("------+--");
+        
+        // Probar las 8 combinaciones posibles
+        {a,b,c} = 3'b000; #10; $display("%b %b %b | %b", a, b, c, y);
+        {a,b,c} = 3'b001; #10; $display("%b %b %b | %b", a, b, c, y);
+        {a,b,c} = 3'b010; #10; $display("%b %b %b | %b", a, b, c, y);
+        {a,b,c} = 3'b011; #10; $display("%b %b %b | %b", a, b, c, y);
+        {a,b,c} = 3'b100; #10; $display("%b %b %b | %b", a, b, c, y);
+        {a,b,c} = 3'b101; #10; $display("%b %b %b | %b", a, b, c, y);
+        {a,b,c} = 3'b110; #10; $display("%b %b %b | %b", a, b, c, y);
+        {a,b,c} = 3'b111; #10; $display("%b %b %b | %b", a, b, c, y);
+        
+        $finish;
+    end
+endmodule
